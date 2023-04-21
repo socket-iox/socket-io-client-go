9848696358a858f4e91491634b86c794b3e5562e
SSebo <ssebo.zzz@gmail.com>
2023-04-21 15:19:47 +0800

    feat: support basic socket.io golang client

---
+++ client.go
@@ -0,0 +1,153 @@
+package socketio_client
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/cenkalti/backoff"
+	"log"
+	"net/url"
+	transport "socket-io-client-go/transport"
+	"sync"
+	"time"
+)
+
+type Client struct {
+	NameSpace *string
+	url       string
+	transport string
+	stream    transport.Stream
+	handlers  map[string]func(client *Client, data []string)
+	mutex     sync.RWMutex
+	isClosed  bool
+}
+
+func (c *Client) Connect(urlStr string, transport string) error {
+	u, err := url.Parse(urlStr)
+	if err != nil {
+		return err
+	}
+	if len(u.Path) == 0 {
+		u.Path = "socket.io"
+	}
+	c.url = fmt.Sprintf("%s", u)
+	c.isClosed = false
+	c.transport = transport
+	return c.doConnect(false)
+}
+
+func (c *Client) doConnect(reConnect bool) error {
+	if reConnect {
+		c.stream.Close()
+		c.handleEvent("disconnect", []string{})
+	}
+
+	switch c.transport {
+	case "polling":
+		polling := transport.NewPolling(c.url, c.NameSpace)
+		c.stream = &polling
+	case "websocket":
+		websocket := transport.NewWebsocket(c.url, c.NameSpace)
+		c.stream = &websocket
+	default:
+		polling := transport.NewPolling(c.url, c.NameSpace)
+		c.stream = &polling
+	}
+
+	if err := c.stream.Open(); err != nil {
+		return err
+	}
+	go c.handleIncoming()
+	return nil
+}
+
+func (c *Client) DisConnect() {
+	c.isClosed = true
+	c.stream.Close()
+}
+
+func (c *Client) Emit(name string, payload interface{}) error {
+	msg, err := encode(name, c.NameSpace, payload)
+	if err != nil {
+		return err
+	}
+	return c.stream.Write(*msg)
+}
+
+func (c *Client) On(name string, handler func(client *Client, data []string)) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	if c.handlers == nil {
+		c.handlers = make(map[string]func(client *Client, data []string))
+	}
+
+	c.handlers[name] = handler
+	return
+}
+
+func encode(name string, nameSpace *string, payload interface{}) (*string, error) {
+	message := "42"
+	if nameSpace != nil {
+		message += *nameSpace + ","
+	}
+
+	if payload != nil {
+		p, err := json.Marshal(&payload)
+		if err != nil {
+			return nil, err
+		}
+
+		message += "[\"" + name + "\"," + string(p) + "]"
+	} else {
+		message += "[\"" + name + "\"]"
+	}
+
+	return &message, nil
+}
+
+func (c *Client) handleIncoming() {
+
+	for {
+		if c.isClosed {
+			break
+		}
+
+		msgs, err := c.stream.Read()
+		if err != nil {
+			go c.reConnect()
+			break
+		}
+
+		if len(msgs) == 0 {
+			continue
+		}
+
+		c.handleEvent(msgs[0], msgs[1:])
+	}
+
+}
+
+func (c *Client) handleEvent(name string, msgs []string) {
+
+	c.mutex.RLock()
+	handler, ok := c.handlers[name]
+	c.mutex.RUnlock()
+
+	if ok {
+		handler(c, msgs)
+	}
+}
+
+func (c *Client) reConnect() {
+	b := backoff.NewExponentialBackOff()
+	b.MaxElapsedTime = 10 * time.Minute
+
+	err := backoff.Retry(
+		func() error {
+			return c.doConnect(true)
+		},
+		b)
+	if err != nil {
+		log.Fatalf("error after retrying: %v", err)
+	}
+}
---
+++ transport/websocket.go
@@ -0,0 +1,144 @@
+package transport
+
+import (
+	"encoding/json"
+	"errors"
+	"github.com/gorilla/websocket"
+	"io"
+	"net/http"
+	"strings"
+)
+
+type Websocket struct {
+	conn      *websocket.Conn
+	url       string
+	nameSpace *string
+	sid       string
+	isClosed  bool
+	params    *TransportParams
+
+	recv chan []string
+	send chan []string
+}
+
+func NewWebsocket(url string, nameSpace *string) Websocket {
+	url = strings.Replace(url, "http", "ws", 1)
+	return Websocket{
+		url:       url + "?EIO=4&transport=websocket",
+		nameSpace: nameSpace,
+		isClosed:  false,
+		recv:      make(chan []string, ChannelBufferSize),
+		send:      make(chan []string, ChannelBufferSize)}
+}
+
+func (w *Websocket) Open() error {
+	dialer := websocket.Dialer{}
+	conn, _, err := dialer.Dial(w.url, http.Header{})
+	if err != nil {
+		return err
+	}
+	w.conn = conn
+
+	msgType, reader, err := w.conn.NextReader()
+	if err != nil {
+		return err
+	}
+	if msgType != websocket.TextMessage {
+		return errors.New("open packet should be text message")
+	}
+
+	data, err := io.ReadAll(reader)
+	if err != nil {
+		return err
+	}
+
+	body := string(data)
+
+	if string(body[0]) != OpenPacket {
+		return errors.New("first should be open packet")
+	}
+
+	// decode params
+	var params TransportParams
+	if err := json.Unmarshal([]byte(body[1:]), &params); err != nil {
+		return err
+	}
+	w.params = &params
+
+	// open namespace
+	var openMsg string
+	if w.nameSpace != nil {
+		openMsg = OpenNamespace + *w.nameSpace + ","
+	} else {
+		openMsg = OpenNamespace
+	}
+	err = w.Write(openMsg)
+	if err != nil {
+		return err
+	}
+
+	w.recv <- []string{"connect"}
+
+	return nil
+}
+
+func (w *Websocket) Read() (message []string, err error) {
+	select {
+	case message = <-w.recv:
+		return message, nil
+	default:
+	}
+
+	msgType, reader, err := w.conn.NextReader()
+	if err != nil {
+		return nil, err
+	}
+	if msgType != websocket.TextMessage {
+		return nil, nil
+	}
+
+	data, err := io.ReadAll(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	payloadStr := string(data)
+
+	if payloadStr == PingPacket {
+		w.Write(PongPacket)
+	}
+
+	if strings.HasPrefix(payloadStr, NewMessage) {
+		payloadStr = payloadStr[2:]
+		if strings.Contains(payloadStr, *w.nameSpace) {
+			payloadStr = strings.Split(payloadStr, *w.nameSpace+",")[1]
+		}
+		var message []string
+		if err := json.Unmarshal([]byte(payloadStr), &message); err != nil {
+			return nil, err
+		}
+		return message, nil
+	}
+
+	return nil, nil
+}
+
+func (w *Websocket) Write(message string) error {
+	if w.conn == nil {
+		return errors.New("websocket is not open")
+	}
+
+	writer, err := w.conn.NextWriter(websocket.TextMessage)
+	if err != nil {
+		return err
+	}
+
+	if _, err := writer.Write([]byte(message)); err != nil {
+		return err
+	}
+
+	return writer.Close()
+}
+func (w *Websocket) Close() {
+
+}
